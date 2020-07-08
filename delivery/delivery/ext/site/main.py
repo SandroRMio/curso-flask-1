9aaad7ec7a82ecc4f2d2c4eabef0b95ccac431c3
Sandro Roberto Mio <sandrormio@gmail.com>
2020-07-08 10:42:20 -0300

    adicionando templates e terminando a configuracao

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -1,22 +1,21 @@
 from flask import render_template
 from flask import Blueprint
+from flask import current_app
 
 bp = Blueprint("site", __name__)
 
 
 @bp.route("/")
 def index():
-
+    current_app.logger.debug("tretpreutertiert")
     return render_template("index.html")
 
 
 @bp.route("/conheca")
 def about():
-
     return render_template("about.html")
 
 
 @bp.route("/restauracao")
 def restaurants():
-
     return render_template("restaurants.html")
