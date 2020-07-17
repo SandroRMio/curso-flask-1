dea957b32fd19c73ac7afaa24ad187d3de8b0bc2
Sandro Roberto Mio <sandrormio@gmail.com>
2020-07-17 17:02:52 -0300

    criando e populando tabelas

--- delivery/delivery/ext/cli/__init__.py
+++ delivery/delivery/ext/cli/__init__.py
@@ -11,6 +11,38 @@ def init_app(app):
         db.create_all()
 
     @app.cli.command()
+    @click.option("--name", "-n")
+    def add_category(name):
+        """adiciona um nova categoria..."""
+        category = models.Category(
+            name=name
+        )
+        db.session.add(category)
+        db.session.commit()
+
+        click.echo(f" Categoria {name} criado com sucesso  !!!")
+
+    # flask add-user --email=sandrormio@com --passw=222
+
+    @app.cli.command()
+    @click.option("--zip", "-z")
+    @click.option("--country", "-c")
+    @click.option("--address", "-a")
+    @click.option("--user_id", "-u")
+    def add_address(zip, country, address, user_id):
+        """adiciona um novo endereço"""
+        address = models.Address(
+            zip=zip,
+            country=country,
+            address=address,
+            user_id=user_id
+        )
+        db.session.add(address)
+        db.session.commit()
+
+        click.echo(f" Endereço {address} criado com sucesso  !!!")
+
+    @app.cli.command()
     @click.option("--email", "-e")
     @click.option("--passwd", "-p")
     @click.option("--admin", "-a", is_flag=True, default=False)
