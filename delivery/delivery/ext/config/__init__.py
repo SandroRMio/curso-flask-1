2acdc75e4308eaadb4bcdd1e6c406acc16d6153a
Sandro Roberto Mio <sandrormio@gmail.com>
2020-08-14 22:58:36 -0300

    Dynaconf
    cofigurando o dynaconf....

--- delivery/delivery/ext/config/__init__.py
+++ delivery/delivery/ext/config/__init__.py
@@ -1,10 +1,5 @@
-def init_app(app):
-    app.config["SECRET_KEY"] = "dogcat11"
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///del_foods.db'
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
+from dynaconf import FlaskDynaconf
 
-    if app.debug:
-        app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
-        app.config['DEBUG_TB_PROFILER_ENABLED'] = True
-        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+def init_app(app):
+    FlaskDynaconf(app)
